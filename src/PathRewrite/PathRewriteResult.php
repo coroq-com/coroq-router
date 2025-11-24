7f155edb9abe73b4e6c740071b5e279d8f90c3bd
Ozawa Masayuki <ozawa@coroq.com>
2025-11-24 18:14:58 +0900

    Fix php 8.0 incompatibility

--- src/PathRewrite/PathRewriteResult.php
+++ src/PathRewrite/PathRewriteResult.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 namespace Coroq\Router\PathRewrite;
 
 class PathRewriteResult {
+  public string $path;
+  /** @var array<string, string> */
+  public array $params;
+
   /**
    * @param array<string, string> $params
    */
-  public function __construct(
-    public readonly string $path,
-    public readonly array $params,
-  ) {}
+  public function __construct(string $path, array $params) {
+    $this->path = $path;
+    $this->params = $params;
+  }
 }
